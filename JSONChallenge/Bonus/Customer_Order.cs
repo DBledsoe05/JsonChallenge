970c47aa9bc42e6d7ea2be1313cde3f9d0a4222b
Denzel <dbledsoe374@gmail.com>
2021-07-27 21:30:53 -0400

    First commit input.

---
+++ JSONChallenge/Bonus/Customer_Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge.Bonus
+{
+    class Customer_Order
+    {
+        public string OrderId { get; set; }
+        public int MyProperty { get; set; }
+        public List<Customer> Order { get; set; }
+        public List<LineItems> Items { get; set; }
+
+    }
+}
---
+++ JSONChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.FirstName = "Denzel";
+            person.LastName = "Bledsoe";
+            person.DOB = new DateTime(1991, 2, 11);
+            person.Favorites = new List<string> { "Games", "Movies"};
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
